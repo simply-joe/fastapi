c198f6af94e46c9e3c9e32a33969f135ae7c7f0f
Joe Krawiec <jkrawiec@digitalocean.com>
2020-11-19 15:56:29 -0500

    Item Class refs

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, item: Item):
+    return {"item_id": item_id, "q": q, "item_name": item.name}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
