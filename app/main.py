84ade5790b56d75d6b3c7442a784ec226e21e8d2
Joe Krawiec <jkrawiec@digitalocean.com>
2020-11-19 16:01:34 -0500

    rm example query

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, item: Item):
-    return {"item_id": item_id, "q": q, "item_name": item.name}
+    return {"item_id": item_id, "item_name": item.name}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
