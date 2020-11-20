becb1425ecc80666d6b756f350ff0f19bcf82be8
Joe Krawiec <jkrawiec@digitalocean.com>
2020-11-20 10:00:52 -0500

    up date

--- app/main.py
+++ app/main.py
@@ -15,13 +15,9 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, item: Item):
-    return {"item_id": item_id, "item_name": item.name}
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_id": item_id, "item_name": item.name}
-
-@app.get("/stores/{store_id}")
-def read_store(store_id: int, q: str = None):
-    return {"store_id": store_id, "q": q}
